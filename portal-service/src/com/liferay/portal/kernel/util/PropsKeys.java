ef7455c4c681b59d21b93bd150f9af2540b29e16
sherryre <sherry.yang@liferay.com>
2012-10-24 14:30:29 -0700

    LPS-30653 Increasing friendly url max length from 255 characters to 4096.

--- portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1123,6 +1123,8 @@ public interface PropsKeys {
 
 	public static final String LAYOUT_FRIENDLY_URL_KEYWORDS = "layout.friendly.url.keywords";
 
+	public static final String LAYOUT_FRIENDLY_URL_MAX_LENGTH = "layout.friendly.url.max.length";
+
 	public static final String LAYOUT_FRIENDLY_URL_PAGE_NOT_FOUND = "layout.friendly.url.page.not.found";
 
 	public static final String LAYOUT_FRIENDLY_URL_PRIVATE_GROUP_SERVLET_MAPPING = "layout.friendly.url.private.group.servlet.mapping";
