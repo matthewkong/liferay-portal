d4b36a5be3d02ab003ff51d72ce701b475f1852e
Jonathan McCann <jonathan.mccann@liferay.com>
2012-12-11 13:48:12 -0800

    LPS-31548 The locale should not be taken from the path

--- portal-impl/src/com/liferay/portal/servlet/filters/i18n/I18nFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/i18n/I18nFilter.java
@@ -145,9 +145,7 @@ public class I18nFilter extends BasePortalFilter {
 			}
 		}
 
-		Locale i18nPathLocale = LocaleUtil.fromLanguageId(i18nPathLanguageId);
-
-		if (!LanguageUtil.isAvailableLocale(i18nPathLocale)) {
+		if (!LanguageUtil.isAvailableLocale(locale)) {
 			return null;
 		}
 
