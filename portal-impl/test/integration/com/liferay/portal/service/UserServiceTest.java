c3c0a716a37897e26c2c5d92786d137227b5b31d
HSchueler <james.schueler@liferay.com>
2013-05-31 10:06:16 -0700

    LPS-35417 Integration Tests

--- portal-impl/test/integration/com/liferay/portal/service/UserServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/UserServiceTest.java
@@ -14,14 +14,22 @@
 
 package com.liferay.portal.service;
 
+import com.liferay.portal.ReservedUserEmailAddressException;
 import com.liferay.portal.kernel.dao.orm.FinderCacheUtil;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
 import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ReflectionUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.Address;
+import com.liferay.portal.model.EmailAddress;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Organization;
+import com.liferay.portal.model.Phone;
 import com.liferay.portal.model.User;
+import com.liferay.portal.model.UserGroupRole;
+import com.liferay.portal.model.Website;
+import com.liferay.portal.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.security.permission.PermissionThreadLocal;
@@ -30,10 +38,16 @@ import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import com.liferay.portal.test.TransactionalCallbackAwareExecutionTestListener;
 import com.liferay.portal.util.GroupTestUtil;
 import com.liferay.portal.util.OrganizationTestUtil;
+import com.liferay.portal.util.PropsValues;
 import com.liferay.portal.util.TestPropsValues;
 import com.liferay.portal.util.UserTestUtil;
+import com.liferay.portlet.announcements.model.AnnouncementsDelivery;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 import org.junit.Assert;
@@ -68,6 +82,88 @@ public class UserServiceTest {
 		User user = addUser();
 
 		UserServiceUtil.deleteUser(user.getUserId());
+	}
+
+	@Test
+	public void testEmailSecurityAddUserWithWorkFlow() throws Exception {
+
+		Field field = getField("COMPANY_SECURITY_STRANGERS_WITH_MX");
+
+		Object value = field.get(null);
+
+		long temp = Long.valueOf(PrincipalThreadLocal.getName());
+		PrincipalThreadLocal.setName(0);
+
+		try {
+			field.set(null, Boolean.FALSE);
+
+			User user = addUserWithWorkFlow();
+
+			Assert.fail();
+		}
+		catch (ReservedUserEmailAddressException rueae) {}
+
+		field.set(null, Boolean.TRUE);
+
+		PrincipalThreadLocal.setName(temp);
+	}
+
+	@Test
+	public void testEmailSecurityUpdateEmailAddress() throws Exception {
+
+		Field field = getField("COMPANY_SECURITY_STRANGERS_WITH_MX");
+
+		Object value = field.get(null);
+
+		try {
+			field.set(null, Boolean.FALSE);
+
+			User user = addUser();
+			user.setEmailAddress("testcompanymx@test.com");
+			UserLocalServiceUtil.updateUser(user);
+
+			long userId = user.getUserId();
+
+			String emailAddress = "testcompanymx@liferay.com";
+
+			UserServiceUtil.updateEmailAddress(
+					userId, user.getPassword(), emailAddress, emailAddress,
+					new ServiceContext());
+
+			Assert.fail();
+		}
+		catch (ReservedUserEmailAddressException rueae) {}
+
+		field.set(null, Boolean.TRUE);
+	}
+
+	@Test
+	public void testEmailUpdateUser() throws Exception {
+
+		Field field = getField("COMPANY_SECURITY_STRANGERS_WITH_MX");
+
+		Object value = field.get(null);
+
+		long temp = Long.valueOf(PrincipalThreadLocal.getName());
+		PrincipalThreadLocal.setName(0);
+
+		try {
+			field.set(null, Boolean.FALSE);
+
+			User user = addUser();
+			user.setEmailAddress("testcompanymx@test.com");
+			UserLocalServiceUtil.updateUser(user);
+
+			updateUser(user);
+
+			Assert.fail();
+
+		}
+		catch (ReservedUserEmailAddressException rueae) {}
+
+		field.set(null, Boolean.TRUE);
+
+		PrincipalThreadLocal.setName(temp);
 	}
 
 	@Test
@@ -359,6 +455,59 @@ public class UserServiceTest {
 			organizationIds, roleIds, userGroupIds, sendMail, serviceContext);
 	}
 
+	protected User addUserWithWorkFlow() throws Exception {
+		boolean autoPassword = true;
+		String password1 = StringPool.BLANK;
+		String password2 = StringPool.BLANK;
+		boolean autoScreenName = true;
+		String screenName = StringPool.BLANK;
+		String emailAddress =
+			"UserServiceTest." + ServiceTestUtil.nextLong() + "@liferay.com";
+		long facebookId = 0;
+		String openId = StringPool.BLANK;
+		Locale locale = LocaleUtil.getDefault();
+		String firstName = "UserServiceTest";
+		String middleName = StringPool.BLANK;
+		String lastName = "UserServiceTest";
+		int prefixId = 0;
+		int suffixId = 0;
+		boolean male = true;
+		int birthdayMonth = Calendar.JANUARY;
+		int birthdayDay = 1;
+		int birthdayYear = 1970;
+		String jobTitle = StringPool.BLANK;
+		long[] groupIds = null;
+		long[] organizationIds = null;
+		long[] roleIds = null;
+		long[] userGroupIds = null;
+		boolean sendMail = false;
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		return UserServiceUtil.addUserWithWorkflow(
+			TestPropsValues.getCompanyId(), autoPassword, password1, password2,
+			autoScreenName, screenName, emailAddress, facebookId, openId,
+			locale, firstName, middleName, lastName, prefixId, suffixId, male,
+			birthdayMonth, birthdayDay, birthdayYear, jobTitle, groupIds,
+			organizationIds, roleIds, userGroupIds, sendMail, serviceContext);
+	}
+
+	protected Field getField(String fieldName) throws Exception {
+		Field field = ReflectionUtil.getDeclaredField(
+				PropsValues.class, fieldName);
+
+		int modifiers = field.getModifiers();
+
+		if ((modifiers & Modifier.FINAL) == Modifier.FINAL) {
+			Field modifiersField = ReflectionUtil.getDeclaredField(
+					Field.class, "modifiers");
+
+			modifiersField.setInt(field, modifiers & ~Modifier.FINAL);
+		}
+
+		return field;
+	}
+
 	protected void unsetGroupUsers(
 				long groupId, User subjectUser, User objectUser)
 		throws Exception {
@@ -385,6 +534,66 @@ public class UserServiceTest {
 
 		UserServiceUtil.unsetOrganizationUsers(
 			organizationId, new long[] {objectUser.getUserId()});
+	}
+
+	protected User updateUser(User user) throws Exception {
+		String oldPassword = StringPool.BLANK;
+		String newPassword1 =StringPool.BLANK;
+		String newPassword2 =StringPool.BLANK;
+		Boolean passwordReset = false;
+		String reminderQueryQuestion = StringPool.BLANK;
+		String reminderQueryAnswer = StringPool.BLANK;
+		String screenName = "TestUser" + ServiceTestUtil.nextLong();
+		String emailAddress =
+			"UserServiceTest." + ServiceTestUtil.nextLong() + "@liferay.com";
+		long facebookId = 0;
+		String openId = StringPool.BLANK;
+		String languageId = StringPool.BLANK;
+		String timeZoneId = StringPool.BLANK;
+		String greeting = StringPool.BLANK;
+		String comments = StringPool.BLANK;
+		String firstName = "UserServiceTest";
+		String middleName = StringPool.BLANK;
+		String lastName = "UserServiceTest";
+		int prefixId = 0;
+		int suffixId = 0;
+		boolean male = true;
+		int birthdayMonth = Calendar.JANUARY;
+		int birthdayDay = 1;
+		int birthdayYear = 1970;
+		String smsSn = StringPool.BLANK;
+		String aimSn = StringPool.BLANK;
+		String facebookSn = StringPool.BLANK;
+		String icqSn = StringPool.BLANK;
+		String jabberSn = StringPool.BLANK;
+		String msnSn = StringPool.BLANK;
+		String mySpaceSn = StringPool.BLANK;
+		String skypeSn = StringPool.BLANK;
+		String twitterSn = StringPool.BLANK;
+		String ymSn = StringPool.BLANK;
+		String jobTitle = StringPool.BLANK;
+		long[] groupIds = null;
+		long[] organizationIds = null;
+		long[] roleIds = null;
+		List<UserGroupRole> userGroupRoles = null;
+		long[] userGroupIds = null;
+		List<Address> addresses = null;
+		List<EmailAddress> emailAddresses = null;
+		List<Phone> phones = null;
+		List<Website> websites = null;
+		List<AnnouncementsDelivery> announcementsDeliveries = null;
+		ServiceContext serviceContext = new ServiceContext();
+
+		return UserServiceUtil.updateUser(
+			user.getUserId(), oldPassword, newPassword1, newPassword2,
+			passwordReset, reminderQueryQuestion, reminderQueryAnswer,
+			screenName, emailAddress, facebookId, openId, languageId,
+			timeZoneId, greeting, comments, firstName, middleName, lastName,
+			prefixId, suffixId, male, birthdayMonth, birthdayDay, birthdayYear,
+			smsSn, aimSn, facebookSn, icqSn, jabberSn, msnSn, mySpaceSn,
+			skypeSn, twitterSn, ymSn, jobTitle, groupIds, organizationIds,
+			roleIds, userGroupRoles, userGroupIds, addresses, emailAddresses,
+			phones, websites, announcementsDeliveries, serviceContext);
 	}
 
 }
