dbc441eecc94f4c2a937196a186f629f569b1017
Preston-Crary <preston.crary@liferay.com>
2013-08-14 10:19:30 -0700

    LPS-38634 Update the rest of the portal removing unessessary logic involving the property

--- portal-impl/src/com/liferay/portal/action/LoginAction.java
+++ portal-impl/src/com/liferay/portal/action/LoginAction.java
@@ -15,12 +15,9 @@
 package com.liferay.portal.action;
 
 import com.liferay.portal.kernel.portlet.WindowStateFactory;
-import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.theme.ThemeDisplay;
@@ -69,21 +66,6 @@ public class LoginAction extends Action {
 			return null;
 		}
 
-		if (PropsValues.COMPANY_SECURITY_AUTH_REQUIRES_HTTPS &&
-			!request.isSecure()) {
-
-			StringBundler sb = new StringBundler(4);
-
-			sb.append(PortalUtil.getPortalURL(request, true));
-			sb.append(request.getRequestURI());
-			sb.append(StringPool.QUESTION);
-			sb.append(request.getQueryString());
-
-			response.sendRedirect(sb.toString());
-
-			return null;
-		}
-
 		String login = ParamUtil.getString(request, "login");
 		String password = request.getParameter("password");
 		boolean rememberMe = ParamUtil.getBoolean(request, "rememberMe");
@@ -102,25 +84,6 @@ public class LoginAction extends Action {
 			if (PropsValues.PORTAL_JAAS_ENABLE) {
 				return actionMapping.findForward("/portal/touch_protected.jsp");
 			}
-
-			String redirect = ParamUtil.getString(request, "redirect");
-
-			redirect = PortalUtil.escapeRedirect(redirect);
-
-			if (Validator.isNull(redirect)) {
-				redirect = themeDisplay.getPathMain();
-			}
-
-			if (redirect.charAt(0) == CharPool.SLASH) {
-				String portalURL = PortalUtil.getPortalURL(
-					request, request.isSecure());
-
-				if (Validator.isNotNull(portalURL)) {
-					redirect = portalURL.concat(redirect);
-				}
-			}
-
-			response.sendRedirect(redirect);
 
 			return null;
 		}
@@ -145,13 +108,11 @@ public class LoginAction extends Action {
 		}
 
 		if (PropsValues.COMPANY_SECURITY_AUTH_REQUIRES_HTTPS) {
-			String portalURL = PortalUtil.getPortalURL(request);
+			String portalURL = PortalUtil.getPortalURL(request, false);
 			String portalURLSecure = PortalUtil.getPortalURL(request, true);
 
-			if (!portalURL.equals(portalURLSecure)) {
-				redirect = StringUtil.replaceFirst(
-					redirect, portalURL, portalURLSecure);
-			}
+			redirect = StringUtil.replaceFirst(
+				redirect, portalURL, portalURLSecure);
 		}
 
 		String loginRedirect = ParamUtil.getString(request, "redirect");
